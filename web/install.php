608e74c4b3432f72bb6ca9028cfdc5ec79b89580
Ousama Ben Younes <contact@lesmuslims.fr>
2014-12-05 08:52:51 +0100

    [Refacto] updated wpcc source organisation

--- web/install.php
+++ web/install.php
@@ -7,21 +7,25 @@ try {
 
     if (0 !== sizeof($_POST))
     {
-
         $nextStep = $_POST['nextStep'];
+        //  Service Configuration Form 1/2
         if ('configureServicesForm' === $nextStep
                 && 0 === sizeof($webParsingConfig)) {
             $ui = new wpccUi();
             $ui->configureProjectGenerate($_POST);
             $ui->configureServicesForm();
         }
+        //  Service Configuration Form 2/2
         if ('configureServicesFormStep2' === $nextStep
             && 0 === sizeof($webParsingConfig)) {
             $ui = new wpccUi($_POST["service"], $_POST["nbConfig"]);
             $ui->configureServicesFormStep2();
         }
+        if ('configureServicesFormGenerate' === $nextStep) {
 
+        }
     } else {
+        // Main configuration Form - First Step
         $ui = new wpccUi();
         $ui->configureProjectForm();
     }
