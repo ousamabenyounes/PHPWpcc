eb858bd4935df8889ff01958182091addd08ef24
Ousama Ben Younes <contact@lesmuslims.fr>
2015-05-26 01:34:42 +0200

    [fix] updating initial phpwpcc configuration

--- config/wpcc_groupurl.php
+++ config/wpcc_groupurl.php
@@ -1,40 +1,22 @@
 <?php
-$groupUrl = array(
-   "Epitech" => array(
-    
-                    "http://www.epitech.fr" => array(
-                         "http://www.epitech.fr" => array(  ),
-                        ),
-        
-
-
-   ),
-   "Google" => array(
-    
-                    "http://www.google.fr" => array(
-                         "http://www.google.fr" => array(  ),
-                        ),
-        
-
-
-   ),
-   "leclubfrancetelevisions" => array(
-    
-                    "http://www.leclubfrancetelevisions.fr" => array(
-                         "http://www.leclubfrancetelevisions.fr" => array(  "pub", "audience", ),
-                        ),
-        
-
-
-   ),
-   "culturebox" => array(
-    
-                    "http://www.francetv.fr/culturebox" => array(
-                         "http://www.francetv.fr/culturebox" => array(  "pub", "audience", ),
-                        ),
-        
-
-
-   ),
+    $groupUrl = array (
+  'Epitech' => 
+  array (
+    'http://www.epitech.fr' => 
+    array (
+      'http://www.epitech.fr' => 
+      array (
+      ),
+    ),
+  ),
+  'Google' => 
+  array (
+    'http://www.google.fr' => 
+    array (
+      'http://www.google.fr' => 
+      array (
+      ),
+    ),
+  ),
 );
 ?>
--- config/wpcc_services.php
+++ config/wpcc_services.php
@@ -1,21 +1,9 @@
 <?php
 $servicesConfig = array(
-  "Pub" => array(
+  "Jquery" => array(
        'acceptedConfig' => array(
-               	 "minCurrent" => array(
-	    	       "http://static.francetv.fr/js/publicite-min.js", 
-	    	   
-         ),
-               	 "minAsyncCurrent" => array(
-	    	       "http://static.francetv.fr/js/publicite-async-min.js", 
-	    	   
-         ),
-      ),
-),
-  "Audience" => array(
-       'acceptedConfig' => array(
-               	 "minCurrent" => array(
-	    	       "http://static.francetv.fr/js/audience-min.js", 
+               	 "Jquery" => array(
+	    	       "Jquery", 
 	    	   
          ),
       ),
