de195765a19fe62ff0fc9ea93c7a43a06c1f6f58
Ousama Ben Younes <contact@lesmuslims.fr>
2015-05-26 00:48:23 +0200

    [fix] fixed bad git ignore path

---
+++ config/wpcc_purge.php
@@ -0,0 +1,18 @@
+<?php
+$purgeConfig = array (
+  'wpcc_config' => 
+  array (
+
+  ),
+  'wpcc_groupurl' => 
+  array (
+  ),
+  'wpcc_services' => 
+  array (
+  ),
+  'wpcc_cache' => 
+  array (
+
+  ),
+);
+?>
